9d6db707cc0b0d906aa8eacee8b4e771fad0134c
GerardoM3 <gerardo.monroy21@itca.edu.sv>
2022-06-06 13:37:35 -0600

    Condiciones en dart

---
+++ bin/main.dart
@@ -0,0 +1,35 @@
+void main(List<String> args) {
+  /*-----------------------------------------
+  "Ejemplo de si y si no" 
+  
+  var precio = 500;
+  if (precio == 500) {
+    print('Precio promedio');
+    }else if (precio > 500){
+      print('Precio mayor del promedio');
+      }else {
+        print('Precio comun');
+      }
+    }*/
+  /*
+---------------------------------------------
+  "Ejemplo de Segun que"
+  var number = 10;
+  switch (number) {
+    case 10:
+      print('Es un Dies!');
+
+      break;
+    case 20:
+      print('Es un Veinte!');
+
+      break;
+    case 30:
+      print('Es un Trienta!');
+      break;
+    default:
+      print('No es un valor correcto');
+      break;
+  }
+  -------------------------------------------*/
+}
